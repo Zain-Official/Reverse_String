0ec19e5995d5560e7297fd9a6ef00a5018e00202
Zain <zainofficial03@gmail.com>
2021-05-26 13:22:16 +0500

    Add project files.

---
+++ Reverse_String/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name Or Favourite Quote");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("You Entered : {0}",str);
+            Char[] ch = str.ToCharArray();
+            Console.WriteLine("\n**************String After Reverse************\n");
+
+            int Counter = 0;
+
+            for(int i=ch.Length-1; i>0; i--)
+            {
+                if (ch[i] !=' ')
+                {
+                    Counter++;
+                }
+                else if (ch[i] == ' ')
+                {
+                    for (int j =i+1; Counter > 0; j++,Counter--)
+                    {
+                        Console.Write(ch[j]);
+                    }
+                    Console.Write(" ");
+                }
+            }
+
+            for(int j=0; j<=Counter; j++)
+            {
+                Console.Write(ch[j]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
